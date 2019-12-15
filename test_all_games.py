5c9e81480d7545b77137b674e14d55b4eb9476a8
gavrilova21 <34915916+gavrilova21@users.noreply.github.com>
2019-12-15 19:28:24 +0300

    Add files via upload

---
+++ test_all_games.py
@@ -0,0 +1,51 @@
+""""
+
+    This is a Regression Test Suite to automatically test all combinations of games and ML frameworks. Each test
+    plays two quick games using an untrained neural network (randomly initialized) against a random player.
+
+    In order for the entire test suite to run successfully, all the required libraries must be installed.  They are:
+    Pytorch, Keras, Tensorflow.
+
+     [ Games ]      Pytorch     Tensorflow  Keras
+      -----------   -------     ----------  -----
+    - Othello       [Yes]       [Yes]       [Yes]
+    - TicTacToe                             [Yes]
+    - Connect4                  [Yes]
+    - Gobang                    [Yes]       [Yes]
+
+"""
+
+import unittest
+
+import Arena
+from MCTS import MCTS
+
+
+
+from connect4.Connect4Game import Connect4Game
+from connect4.Connect4Players import *
+from connect4.tensorflow.NNet import NNetWrapper as Connect4TensorflowNNet
+
+import numpy as np
+from utils import *
+
+class TestAllGames(unittest.TestCase):
+
+    @staticmethod
+    def execute_game_test(game, neural_net):
+        rp = RandomPlayer(game).play
+
+        args = dotdict({'numMCTSSims': 25, 'cpuct': 1.0})
+        mcts = MCTS(game, neural_net(game), args)
+        n1p = lambda x: np.argmax(mcts.getActionProb(x, temp=0))
+
+        arena = Arena.Arena(n1p, rp, game)
+        print(arena.playGames(2, verbose=False))
+
+    def test_connect4_tensorflow(self):
+        self.execute_game_test(Connect4Game(), Connect4TensorflowNNet)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
